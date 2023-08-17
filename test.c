f6b08840a0d10588099b4c2e6036442d833a3ae8
CynicaLion <pranavpnair03@gmail.com>
2023-08-17 19:11:34 +0800

    Commit 2nd message

--- test.c
+++ test.c
@@ -7,6 +7,13 @@ int main() {
     printf("Enter 2 numbers");
     scanf("%d%d",&a,&b);
     printf("The Entered Numbers are %d and %d",a,b);
+    printf("\n");
+    if(a>b){
+      printf(" %d is Greater %d",a,b);  
+    }
+    else{
+        printf(" %d is Greater %d",b,a);  
+    }
 
 
     return 0;
