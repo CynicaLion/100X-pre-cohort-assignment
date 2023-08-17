a15a039057b1e6b262bda3097ad62cd6bc95a4ad
CynicaLion <pranavpnair03@gmail.com>
2023-08-17 19:06:47 +0800

    Commit message

--- test.c
+++ test.c
@@ -1,1 +1,13 @@
- 
+ #include <stdio.h>
+int a;
+int b;
+int main() {
+    
+    printf("Hello world");
+    printf("Enter 2 numbers");
+    scanf("%d%d",&a,&b);
+    printf("The Entered Numbers are %d and %d",a,b);
+
+
+    return 0;
+}
